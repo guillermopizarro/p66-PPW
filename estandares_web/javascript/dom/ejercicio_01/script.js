7ea646f1b7a916b5defd6d621ae8a78c7d587c1b
Guillermo Pizarro <gpizarro@espol.edu.ec>
2025-05-19 20:53:08 -0500

    DOM - CRUD objects.

--- estandares_web/javascript/dom/ejercicio_01/script.js
+++ estandares_web/javascript/dom/ejercicio_01/script.js
@@ -28,6 +28,40 @@ function change_css() {
     title3.style.fontSize = "24px"
 }
 
-let list = document.getElementById("list");
-list.innerHTML += "<li class='item'>Item 4</li>"; // Adds a new list item
+let indice = 4
 
+function add_item() {
+    let list = document.getElementById("list");
+    list.innerHTML += "<li class='item'>Item "+indice+"</li>"; // Adds a new list item
+    indice++
+}
+
+function add_link() {
+    let link = document.createElement("a");
+    link.setAttribute("href", "https://www.example.com");
+    link.innerText = "Visit Example";
+    document.body.appendChild(link);
+}
+
+function create_p() {
+    let newParagraph = document.createElement("p");
+    newParagraph.innerText = "This is a new paragraph.";
+    document.body.appendChild(newParagraph);    
+}
+
+function remove_item() {
+    let listItem = document.querySelector(".item");
+    listItem.remove(); // Removes the first list item
+}
+
+function remove_items() {
+    let listItem = document.querySelectorAll(".item");
+    listItem.forEach(item => item.remove())
+}
+
+function replace_element() {
+    let oldTitle = document.getElementById("title");
+    let newTitle = document.createElement("h1");
+    newTitle.innerText = "New JavaScript DOM Title";
+    oldTitle.replaceWith(newTitle);
+}
