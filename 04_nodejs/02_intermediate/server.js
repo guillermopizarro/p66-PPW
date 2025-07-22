a1b3a497739f90e6affd52246946387613078264
Guillermo Pizarro <gpizarro@espol.edu.ec>
2025-07-21 21:03:09 -0500

    Nodejs intermediate

--- 04_nodejs/02_intermediate/server.js
+++ 04_nodejs/02_intermediate/server.js
@@ -4,13 +4,7 @@ const router = express.Router()
 var app = express()
 app.use( router )
 
-router.get('/', function (req, res){
-    res.send('Hola desde GET.')
-})
 
-router.post('/', function (req, res){
-    res.send('Hola desde POST.')    
-})
 
 let puerto = 3000
 app.listen( puerto )
