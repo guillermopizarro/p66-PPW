dff9ad42d0705eabc28c57c45c5f84215b8e0d1d
Guillermo Pizarro <gpizarro@espol.edu.ec>
2025-07-21 21:29:20 -0500

    Nodejs intermediate

--- 04_nodejs/02_intermediate/server.js
+++ 04_nodejs/02_intermediate/server.js
@@ -1,10 +1,38 @@
 const express = require('express')
+const body_parser = require('body-parser')
 const router = express.Router()
 
 var app = express()
+
+app.use( body_parser.json() )
+app.use( body_parser.urlencoded({extended:false}) )
 app.use( router )
 
+router.get('/mensaje', function(req, res) {
+    console.log( req.headers )
+    res.header({
+        'custom-header':'valor-personalizado'
+    })
+    res.send( 'Lista de mensajes' )
+})
 
+router.post('/', function(req, res) {
+    res.send('Hola desde POST.')
+})
+
+router.delete('/mensaje', function(req, res) {
+    console.log( req.query )
+    console.log( req.body )
+    res.send( 'Mensaje eliminado: ' + req.body.texto1 + ' ' + req.body.texto2 )
+})
+
+router.put('/', function(req, res) {
+    res.send('Hola desde PUT.')
+})
+
+router.patch('/', function(req, res) {
+    res.send('Hola desde PATCH.')
+})
 
 let puerto = 3000
 app.listen( puerto )
