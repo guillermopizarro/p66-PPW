749722c19cff8d2b11329004c97a9dfcc0825a6f
Guillermo Pizarro <gpizarro@espol.edu.ec>
2025-07-28 19:59:58 -0500

    Creación de las funciones en controller y storage.

---
+++ 04_nodejs/03_advanced/basic_architecture/components/user/controller.js
@@ -0,0 +1,24 @@
+const storage = require('./storage')
+
+function addUser( data ) {
+
+}
+
+function getUser( data ) {
+
+}
+
+function updateUser( data ) {
+
+}
+
+function deleteUser( data ) {
+
+}
+
+module.exports = {
+    addUser,
+    getUser,
+    updateUser,
+    deleteUser
+}
---
+++ 04_nodejs/03_advanced/basic_architecture/components/user/storage.js
@@ -0,0 +1,24 @@
+const model = require('./model')
+
+async function addUser( data ) {
+
+}
+
+async function getUser( data ) {
+
+}
+
+async function updateUser( data ) {
+
+}
+
+async function deleteUser( data ) {
+
+}
+
+module.exports = {
+    add:addUser,
+    get:getUser,
+    update:updateUser,
+    delete:deleteUser
+}
